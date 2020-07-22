d42998efc5a24e3589ae76561ff8ab5f7e84bbd4
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-22 17:29:42 +0300

    add import the SQALCHEMI And link mu database to my app

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from . import db
+from flask_sqlalchemy import SQLAlchemy
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+ 
--- manage.py
+++ manage.py
@@ -1,11 +1,25 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
 
+
+
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
+from app.models import User
 
 manager = Manager(app)
+
+
 manager.add_command('server',Server)
 
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=5).run(tests)
+    
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 if __name__ == '__main__':
     manager.run()
