7acde095f0108873ff62cbf9c8a7221d26b17413
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-22 18:41:14 +0300

    Add classes to my models

--- app/models.py
+++ app/models.py
@@ -5,7 +5,38 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    article = db.relationship('Article',backref='user',lazy = 'dynamic')
+    comments = db.relationship('Comment',backref='user',lazy='dynamic')
 
-    def __repr__(self):
-        return f'User {self.username}'
- 
+
+class Article(db.Model):
+    '''
+    This class will contain the database schema for articles table
+    '''
+    __tablename__ = 'articles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    article = db.Column(db.String)
+    category = db.Column(db.String)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+class Comment(db.Model):
+    '''
+    This class will contain the schema for comments
+    '''
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(255))
+    article_id = db.Column(db.Integer,db.ForeignKey('articles.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+
+class Quotes:
+    def __init__(self,author,quote):
+        '''
+        Method to instanciate the quotes class
+        '''
+        self.author = author
+        self.quote = quote
+
+    
