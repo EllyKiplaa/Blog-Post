6308939424a2279538dc5e095550864c69f70657
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-27 00:07:44 +0300

    Fix bug

--- app/email.py
+++ app/email.py
@@ -1,11 +1,11 @@
-from flask_mail import Mail, Message
-from . import mail
-from ..email import mail_message
+# from flask_mail import Mail, Message
+# from . import mail
+# from ..email import mail_message
 
-def mail_message(subject,template,to,**kwargs):
-    sender_email = ellykiplash@gmail.com
+# def mail_message(subject,template,to,**kwargs):
+#     sender_email = ellykiplash@gmail.com
 
-    email = Message(subject, sender=sender_email, recipients=[to])
-    email.body= render_template(template + ".txt",**kwargs)
-    email.html = render_template(template + ".html",**kwargs)
-    mail.send(email)
+#     email = Message(subject, sender=sender_email, recipients=[to])
+#     email.body= render_template(template + ".txt",**kwargs)
+#     email.html = render_template(template + ".html",**kwargs)
+#     mail.send(email)
--- config.py
+++ config.py
@@ -5,13 +5,15 @@ class Config:
     '''
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY="testkeyindevconfig"
+    SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://elly:Access2020@localhost/YuBlog"
 
 class ProdConfig(Config):
     '''
     Production Configurations
     '''
-    SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL')
+    SECRET_KEY="testkeyindevconfig"
+    SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://elly:Access2020@localhost/YuBlog"
 
 class DevConfig(Config):
     '''
